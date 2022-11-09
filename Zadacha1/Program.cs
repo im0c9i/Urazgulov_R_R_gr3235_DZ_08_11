3256d2253422d2fae6b47caa991a24997d93646a
Ramil <ramil-urazgulov444@mail.ru>
2022-11-09 19:24:42 +0700

    Zadacha1

--- Zadacha1/Program.cs
+++ Zadacha1/Program.cs
@@ -9,13 +9,13 @@
 8 4 4 2*/
 int[,] ArrayMatrix(int m, int n)
 {
-    Random rnd = new Random();
+    Random rnd = new();
     int[,] array = new int[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            array[i, j] = rnd.Next(1, 10);
+            array[i, j] = rnd.Next(1, 20);
         }
     }
     return array;
@@ -27,7 +27,7 @@ void PrintArray(int[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.WriteLine($"{array[i, j]} ");
+            Console.Write($"{array[i, j]} ");
         }
         Console.WriteLine();
     }
@@ -44,5 +44,29 @@ int GetNumber(string message)
 int[,] GetArray(int[,] array)
 {
     int[,] resultArray = new int[array.GetLength(0), array.GetLength(1)];
-    
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int z = 0; z < array.GetLength(1) - 1; z++)
+            {
+                
+                if (array[i, z] < array[i, z + 1])
+                {
+                    temp = array[i, z];
+                    array[i, z] = array[i, z + 1];
+                    array[i, z + 1] = temp;
+                }
+            }
+        }
+    }
+    return resultArray;
 }
+
+int m = GetNumber("Введите количество строк");
+int n = GetNumber("Введите количество столбцов");
+int[,] result = ArrayMatrix(m, n);
+PrintArray(result);
+int[,] result2 = GetArray(result);
+PrintArray(result);
